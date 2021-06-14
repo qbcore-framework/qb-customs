99654e89559371f487b604cf3d5bfa11e32bde67
Franco Sanllehi <francosanllehi@gmail.com>
2021-06-14 15:39:08 -0400

    fix(SQL/Sanitize): Fix SQL Injections
    - Comply with https://github.com/qbcore-framework/qb-core/pull/17

--- server/core.lua
+++ server/core.lua
@@ -15,7 +15,7 @@ end)
 
 function IsVehicleOwned(plate)
     local retval = false
-    QBCore.Functions.ExecuteSql(true, "SELECT * FROM `player_vehicles` WHERE `plate` = '"..plate.."'", function(result)
+    QBCore.Functions.ExecuteSql(true, {['a']=plate}, "SELECT * FROM `player_vehicles` WHERE `plate` = @a", function(result)
         if result[1] ~= nil then
             retval = true
         end
@@ -27,7 +27,7 @@ RegisterServerEvent('qb-customs:server:SaveVehicleProps')
 AddEventHandler('qb-customs:server:SaveVehicleProps', function(vehicleProps)
     local src = source
     if IsVehicleOwned(vehicleProps.plate) then
-        QBCore.Functions.ExecuteSql(false, "UPDATE `player_vehicles` SET `mods` = '"..json.encode(vehicleProps).."' WHERE `plate` = '"..vehicleProps.plate.."'")
+        QBCore.Functions.ExecuteSql(false, {['a']=json.encode(vehicleProps),['b']=vehicleProps.plate}, "UPDATE `player_vehicles` SET `mods` = @a WHERE `plate` = @b")
     else
         TriggerClientEvent('QBCore:Notify', src, 'No Owner - Mods Not Saved', 'error')
     end
