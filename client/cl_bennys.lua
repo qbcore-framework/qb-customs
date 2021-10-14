fb50a529215cc0b59b69f48396cd7113305db011
papa bendi <50492505+papabendi@users.noreply.github.com>
2021-10-14 23:40:51 +1300

    able to access menu from any seat in car
    Pull request for #35

--- client/cl_bennys.lua
+++ client/cl_bennys.lua
@@ -838,7 +838,7 @@ Citizen.CreateThread(function()
                     if nearDefault then
                         if not isPlyInBennys then
                             Draw3DText(v.coords.x, v.coords.y, v.coords.z + 0.5, "[Press ~p~E~w~ - Enter Benny's Motorworks]", 255, 255, 255, 255, 4, 0.45, true, true, true, true, 0, 0, 0, 0, 55)
-                            if IsControlJustReleased(1, 38) then
+                            if GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId()), -1) == PlayerPedId() and IsControlJustReleased(1, 38) then
                                 if (v.useJob and isAuthorized((QBCore.Functions.GetPlayerData().job.name), k)) or not v.useJob then
                                     TriggerEvent('event:control:bennys', k)
                                 else
